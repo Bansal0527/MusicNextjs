92e0ebccc2dd446131d7ce94d8da099be09e21fb
Mayank Bansal <mayank.bansal.0527@gmail.com>
2024-08-06 00:37:39 +0530

    instructors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     
     <HeroSection></HeroSection>
     <FeaturedCourses></FeaturedCourses>
+    <WhyChooseUs></WhyChooseUs>
+    <MusicSchoolTestimonials></MusicSchoolTestimonials>
+    <UpcomingWebinars></UpcomingWebinars>
+    <Instructors></Instructors>
     </main>
   );
 }
